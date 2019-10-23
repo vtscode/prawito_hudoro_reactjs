ec7f388413a1983a255e02c4742ad7c8db3b8475
rhivent <zzzzjrjr@gmail.com>
2019-10-23 15:49:56 +0700

    simple-note-firebase:integrate

---
+++ simple-note-firebase/src/config/firebase/index.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD5DKWwji14Nr2X3ZzSypoFK_0sn7oV5Lc",
+    authDomain: "simple-note-firebase-1135c.firebaseapp.com",
+    databaseURL: "https://simple-note-firebase-1135c.firebaseio.com",
+    projectId: "simple-note-firebase-1135c",
+    storageBucket: "simple-note-firebase-1135c.appspot.com",
+    messagingSenderId: "838063527357",
+    appId: "1:838063527357:web:072d760eca0fcc172b6ed4",
+    measurementId: "G-0FDECK0X4X"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+
+export default firebase;
--- simple-note-firebase/src/index.js
+++ simple-note-firebase/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/pages/App';
-
 import * as serviceWorker from './serviceWorker';
+import FirebaseConfig from './config/firebase/index';
+
+console.log('config firebase',FirebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
